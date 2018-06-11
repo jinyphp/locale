1c2a760ed3f0788e58b2088531bc5cfc0f6e8e6f
hojinlee <infohojin@gmail.com>
2018-06-11 18:20:13 +0900

    init

---
+++ data/CultureData.php
@@ -0,0 +1,779 @@
+<?php
+namespace Jiny\Locale;
+
+return [
+	'af-ZA'=>[
+        'Culture'=> "af-ZA",
+        'Display'=> "Afrikaans - South Africa",
+        'Code'=> "0x0436",
+        'ISO'=> "AFK"     
+    ],
+    'sq-AL'=>[
+        'Culture'=> "sq-AL",
+        'Display'=> "Albanian - Albania",
+        'Code'=> "0x041C",
+        'ISO'=> "SQI",
+    ],
+    'ar-DZ'=>[
+        'Culture'=> "ar-DZ",
+		'Display'=> "Arabic - Algeria",
+		'Code'=> "0x1401",
+		'ISO'=> "ARG"
+    ],
+    'ar-BH'=>[
+        'Culture'=> "ar-BH",
+        'Display'=> "Arabic - Bahrain",
+        'Code'=> "0x3C01",
+		'ISO'=> "ARH"
+	],
+    'ar-EG'=>[
+        'Culture'=> "ar-EG",
+        'Display'=> "Arabic - Egypt",
+        'Code'=> "0x0C01",
+        'ISO'=> "ARE"
+    ],
+    'ar-IQ'=>[
+        'Culture'=> "ar-IQ",
+        'Display'=> "Arabic - Iraq",
+        'Code'=> "0x0801",
+        'ISO'=> "ARI"
+    ],
+    'ar-JO'=>[
+        'Culture'=> "ar-JO",
+        'Display'=> "Arabic - Jordan",
+        'Code'=> "0x2C01",
+        'ISO'=> "ARJ"
+    ],
+    'ar-KW'=>[
+        'Culture'=> "ar-KW",
+		'Display'=> "Arabic - Kuwait",
+		'Code'=> "0x3401",
+		'ISO'=> "ARK"
+    ],
+    'ar-LB'=>[
+        'Culture'=> "ar-LB",
+        'Display'=> "Arabic - Lebanon",
+        'Code'=> "0x3001",
+        'ISO'=> "ARB"
+    ],
+    'ar-LY'=>[
+        'Culture'=> "ar-LY",
+        'Display'=> "Arabic - Libya",
+        'Code'=> "0x1001",
+        'ISO'=> "ARL"
+    ],
+    'ar-MA'=>[
+        'Culture'=> "ar-MA",
+        'Display'=> "Arabic - Morocco",
+        'Code'=> "0x1801",
+        'ISO'=> "ARM"
+    ],
+    'ar-OM'=>[
+        'Culture'=> "ar-OM",
+        'Display'=> "Arabic - Oman",
+        'Code'=> "0x2001",
+        'ISO'=> "ARO"
+    ],
+    'ar-QA'=>[
+        'Culture'=> "ar-QA",
+        'Display'=> "Arabic - Qatar",
+        'Code'=> "0x4001",
+        'ISO'=> "ARQ"
+    ],
+    'ar-SA'=>[
+        'Culture'=> "ar-SA",
+        'Display'=> "Arabic - Saudi Arabia",
+        'Code'=> "0x0401",
+        'ISO'=> "ARA"
+    ],
+    'ar-SY'=>[
+        'Culture'=> "ar-SY",
+		'Display'=> "Arabic - Syria",
+		'Code'=> "0x2801",
+		'ISO'=> "ARS"
+    ],
+    'ar-TN'=>[
+        'Culture'=> "ar-TN",
+        'Display'=> "Arabic - Tunisia",
+        'Code'=> "0x1C01",
+        'ISO'=> "ART"
+    ],
+    'ar-AE'=>[
+        'Culture'=> "ar-AE",
+        'Display'=> "Arabic - United Arab Emirates",
+        'Code'=> "0x3801",
+        'ISO'=> "ARU"
+    ],
+    'ar-YE'=>[
+        'Culture'=> "ar-YE",
+        'Display'=> "Arabic - Yemen",
+        'Code'=> "0x2401",
+        'ISO'=> "ARY"
+    ],
+    'hy-AM'=>[
+        'Culture'=> "hy-AM",
+        'Display'=> "Armenian - Armenia",
+        'Code'=> "0x042B"
+    ],
+    'Cy-az-AZ'=>[
+        'Culture'=> "Cy-az-AZ",
+        'Display'=> "Azeri (Cyrillic) - Azerbaijan",
+        'Code'=> "0x082C"
+    ],
+    'Lt-az-AZ'=>[
+        'Culture'=> "Lt-az-AZ",
+        'Display'=> "Azeri (Latin) - Azerbaijan",
+        'Code'=> "0x042C"
+    ],
+    'eu-ES'=>[
+        'Culture'=> "eu-ES",
+        'Display'=> "Basque - Basque",
+        'Code'=> "0x042D",
+        'ISO'=> "EUQ"
+    ],
+    'be-BY'=>[
+        'Culture'=> "be-BY",
+        'Display'=> "Belarusian - Belarus",
+        'Code'=> "0x0423",
+        'ISO'=> "BEL"
+    ],
+	'bg-BG'=>[
+    	'Culture'=> "bg-BG",
+		'Display'=> "Bulgarian - Bulgaria",
+		'Code'=> "0x0402",
+		'ISO'=> "BGR "
+	],
+	'ca-ES'=>[
+    	'Culture'=> "ca-ES",
+		'Display'=> "Catalan - Catalan",
+		'Code'=> "0x0403",
+		'ISO'=> "CAT "
+	],
+	'zh-CN'=>[
+    	'Culture'=> "zh-CN",
+		'Display'=> "Chinese - China",
+		'Code'=> "0x0804",
+		'ISO'=> "CHS"  
+	],
+	'zh-HK'=>[
+    	'Culture'=> "zh-HK",
+		'Display'=> "Chinese - Hong Kong SAR",
+		'Code'=> "0x0C04",
+		'ISO'=> "ZHH"
+	],
+	'zh-MO'=>[
+    	'Culture'=> "zh-MO",
+		'Display'=> "Chinese - Macau SAR",
+		'Code'=> "0x1404"
+	],
+	'zh-SG'=>[
+    	'Culture'=> "zh-SG",
+		'Display'=> "Chinese - Singapore",
+		'Code'=> "0x1004",
+		'ISO'=> "ZHI"  
+	],
+	'zh-TW'=>[
+    	'Culture'=> "zh-TW",
+		'Display'=> "Chinese - Taiwan",
+		'Code'=> "0x0404",
+		'ISO'=> "CHT"
+	],
+	'zh-CHS'=>[
+    	'Culture'=> "zh-CHS",
+		'Display'=> "Chinese (Simplified)",
+		'Code'=> "0x0004"
+	],
+	'zh-CHT'=>[
+    	'Culture'=> "zh-CHT",
+		'Display'=> "Chinese (Traditional)",
+		'Code'=> "0x7C04"
+	],
+	'hr-HR'=>[
+    	'Culture'=> "hr-HR",
+		'Display'=> "Croatian - Croatia",
+		'Code'=> "0x041A",
+		'ISO'=> "HRV"
+	],
+	'cs-CZ'=>[
+    	'Culture'=> "cs-CZ",
+		'Display'=> "Czech - Czech Republic",
+		'Code'=> "0x0405",
+		'ISO'=> "CSY"  
+	],
+	'da-DK'=>[
+    	'Culture'=> "da-DK",
+		'Display'=> "Danish - Denmark",
+		'Code'=> "0x0406",
+		'ISO'=> "DAN"
+	],
+	'div-MV'=>[
+    	'Culture'=> "div-MV",
+		'Display'=> "Dhivehi - Maldives",
+		'Code'=> "0x0465"   
+	],
+	'nl-BE'=>[
+    	'Culture'=> "nl-BE",
+		'Display'=> "Dutch - Belgium",
+		'Code'=> "0x0813",
+		'ISO'=> "NLB" 
+	],
+	'nl-NL'=>[
+    	'Culture'=> "nl-NL",
+		'Display'=> "Dutch - The Netherlands",
+		'Code'=> "0x0413"  
+	],
+	'en-AU'=>[
+    	'Culture'=> "en-AU",
+		'Display'=> "English - Australia",
+		'Code'=> "0x0C09",
+		'ISO'=> "ENA"
+	],
+	'en-BZ'=>[
+    	'Culture'=> "en-BZ",
+		'Display'=> "English - Belize",
+		'Code'=> "0x2809",
+		'ISO'=> "ENL"  
+	],
+	'en-CA'=>[
+    	'Culture'=> "en-CA",
+		'Display'=> "English - Canada",
+		'Code'=> "0x1009",
+		'ISO'=> "ENC"  
+	],
+	'en-CB'=>[
+    	'Culture'=> "en-CB",
+		'Display'=> "English - Caribbean",
+  		'Code'=> "0x2409"    
+	],
+	'en-IE'=>[
+    	'Culture'=> "en-IE",
+		'Display'=> "English - Ireland",
+		'Code'=> "0x1809",
+		'ISO'=> "ENI" 
+	],
+	'en-JM'=>[
+    	'Culture'=> "en-JM",
+		'Display'=> "English - Jamaica",
+		'Code'=> "0x2009",
+		'ISO'=> "ENJ"
+	],
+	'en-NZ'=>[
+    	'Culture'=> "en-NZ",
+		'Display'=> "English - New Zealand",
+		'Code'=> "0x1409",
+		'ISO'=> "ENZ"
+	],
+	'en-PH'=>[
+    	'Culture'=> "en-PH",
+		'Display'=> "English - Philippines",
+		'Code'=> "0x3409" 
+	],
+	'en-ZA'=>[
+    	'Culture'=> "en-ZA",
+		'Display'=> "English - South Africa",
+		'Code'=> "0x1C09",
+		'ISO'=> "ENS"  
+	],
+	'en-TT'=>[
+    	'Culture'=> "en-TT",
+		'Display'=> "English - Trinidad and Tobago",
+  		'Code'=> "0x2C09",
+		'ISO'=> "ENT"  
+	],
+	'en-GB'=>[
+    	'Culture'=> "en-GB",
+		'Display'=> "English - United Kingdom",
+		'Code'=> "0x0809",
+		'ISO'=> "ENG"  
+	],
+	'en-US'=>[
+    	'Culture'=> "en-US",
+		'Display'=> "English - United States",
+		'Code'=> "0x0409",
+		'ISO'=> "ENU"   
+	],
+	'en-ZW'=>[
+    	'Culture'=> "en-ZW",
+		'Display'=> "English - Zimbabwe",
+		'Code'=> "0x3009"   
+	],
+	'et-EE'=>[
+    	'Culture'=> "et-EE",
+		'Display'=> "Estonian - Estonia",
+		'Code'=> "0x0425",
+		'ISO'=> "ETI"   
+	],
+	'fo-FO'=>[
+		'Culture'=> "fo-FO",
+		'Display'=> "Faroese - Faroe Islands",
+		'Code'=> "0x0438",
+		'ISO'=> "FOS"   
+	],
+	'fa-IR'=>[
+		'Culture'=> "fa-IR",
+		'Display'=> "Farsi - Iran",
+		'Code'=> "0x0429",
+		'ISO'=> "FAR"  
+	],
+	'fi-FI'=>[
+		'Culture'=> "fi-FI",
+		'Display'=> "Finnish - Finland",
+		'Code'=> "0x040B",
+		'ISO'=> "FIN" 
+	],
+	'fr-BE'=>[
+		'Culture'=> "fr-BE",
+		'Display'=> "French - Belgium",
+		'Code'=> "0x080C",
+		'ISO'=> "FRB"
+	],
+	'fr-CA'=>[
+		'Culture'=> "fr-CA",
+		'Display'=> "French - Canada",
+		'Code'=> "0x0C0C",
+		'ISO'=> "FRC"
+	],
+	'fr-FR'=>[
+		'Culture'=> "fr-FR",
+		'Display'=> "French - France",
+		'Code'=> "0x040C" 
+	],
+	'fr-LU'=>[
+		'Culture'=> "fr-LU",
+		'Display'=> "French - Luxembourg",
+		'Code'=> "0x140C",
+		'ISO'=> "FRL"  
+	],
+	'fr-MC'=>[
+		'Culture'=> "fr-MC",
+		'Display'=> "French - Monaco",
+		'Code'=> "0x180C" 
+	],
+	'fr-CH'=>[
+		'Culture'=> "fr-CH",
+		'Display'=> "French - Switzerland",
+		'Code'=> "0x100C",
+		'ISO'=> "FRS"
+	],
+	'gl-ES'=>[
+		'Culture'=> "gl-ES",
+		'Display'=> "Galician - Galician",
+		'Code'=> "0x0456"  
+	],
+	'ka-GE'=>[
+		'Culture'=> "ka-GE",
+		'Display'=> "Georgian - Georgia",
+		'Code'=> "0x0437"   
+	],
+	'de-AT'=>[
+		'Culture'=> "de-AT",
+		'Display'=> "German - Austria",
+		'Code'=> "0x0C07",
+		'ISO'=> "DEA"
+	],
+	'de-DE'=>[
+		'Culture'=> "de-DE",
+		'Display'=> "German - Germany",
+		'Code'=> "0x0407"   
+	],
+	'de-LI'=>[
+		'Culture'=> "de-LI",
+		'Display'=> "German - Liechtenstein",
+		'Code'=> "0x1407",
+		'ISO'=> "DEC"
+	],
+	'de-LU'=>[
+		'Culture'=> "de-LU",
+		'Display'=> "German - Luxembourg",
+		'Code'=> "0x1007",
+		'ISO'=> "DEL"
+	],
+	'de-CH'=>[
+		'Culture'=> "de-CH",
+		'Display'=> "German - Switzerland",
+		'Code'=> "0x0807",
+		'ISO'=> "DES"  
+	],
+	'el-GR'=>[
+		'Culture'=> "el-GR",
+		'Display'=> "Greek - Greece",
+		'Code'=> "0x0408",
+		'ISO'=> "ELL"  
+	],
+	'gu-IN'=>[
+		'Culture'=> "gu-IN",
+		'Display'=> "Gujarati - India",
+		'Code'=> "0x0447"   
+	],
+	'he-IL'=>[
+		'Culture'=> "he-IL",
+		'Display'=> "Hebrew - Israel",
+		'Code'=> "0x040D",
+		'ISO'=> "HEB" 
+	],
+	'hi-IN'=>[
+		'Culture'=> "hi-IN",
+		'Display'=> "Hindi - India",
+		'Code'=> "0x0439",
+		'ISO'=> "HIN"  
+	],
+	'hu-HU'=>[
+		'Culture'=> "hu-HU",
+		'Display'=> "Hungarian - Hungary",
+		'Code'=> "0x040E",
+		'ISO'=> "HUN "  
+	],
+	'is-IS'=>[
+		'Culture'=> "is-IS",
+		'Display'=> "Icelandic - Iceland",
+		'Code'=> "0x040F",
+		'ISO'=> "ISL"  
+	],
+	'id-ID'=>[
+		'Culture'=> "id-ID",
+		'Display'=> "Indonesian - Indonesia",
+		'Code'=> "0x0421"   
+	],
+	'it-IT'=>[
+		'Culture'=> "it-IT",
+		'Display'=> "Italian - Italy",
+		'Code'=> "0x0410"  
+	],
+	'it-CH'=>[
+		'Culture'=> "it-CH",
+		'Display'=> "Italian - Switzerland",
+		'Code'=> "0x0810",
+		'ISO'=> "ITS"  
+	],
+	'ja-JP'=>[
+		'Culture'=> "ja-JP",
+		'Display'=> "Japanese - Japan",
+  		'Code'=> "0x0411",
+		'ISO'=> "JPN"
+	],
+	'kn-IN'=>[
+		'Culture'=> "kn-IN",
+		'Display'=> "Kannada - India",
+		'Code'=> "0x044B"   
+	],
+	'kk-KZ'=>[
+		'Culture'=> "kk-KZ",
+		'Display'=> "Kazakh - Kazakhstan",
+		'Code'=> "0x043F"  
+	],
+	'kok-IN'=>[
+		'Culture'=> "kok-IN",
+		'Display'=> "Konkani - India",
+		'Code'=> "0x0457"   
+	],
+	'ko-KR'=>[
+		'Culture'=> "ko-KR",
+		'Display'=> "Korean - Korea",
+		'Code'=> "0x0412",
+		'ISO'=> "KOR"  
+	],
+	'ky-KZ'=>[
+		'Culture'=> "ky-KZ",
+		'Display'=> "Kyrgyz - Kazakhstan",
+		'Code'=> "0x0440"   
+	],
+	'lv-LV'=>[
+		'Culture'=> "lv-LV",
+		'Display'=> "Latvian - Latvia",
+		'Code'=> "0x0426",
+		'ISO'=> "LVI"  
+	],
+	'lt-LT'=>[
+		'Culture'=> "lt-LT",
+		'Display'=> "Lithuanian - Lithuania",
+		'Code'=> "0x0427",
+		'ISO'=> "LTH"
+	], 
+	'mk-MK'=>[
+		'Culture'=> "mk-MK",
+		'Display'=> "Macedonian (FYROM)",
+		'Code'=> "0x042F",
+		'ISO'=> "MKD" 
+	],
+	'ms-BN'=>[
+		'Culture'=> "ms-BN",
+		'Display'=> "Malay - Brunei",
+		'Code'=> "0x083E"   
+	],
+	'ms-MY'=>[
+		'Culture'=> "ms-MY",
+		'Display'=> "Malay - Malaysia",
+		'Code'=> "0x043E"   
+	],
+	'mr-IN'=>[
+		'Culture'=> "mr-IN",
+		'Display'=> "Marathi - India",
+		'Code'=> "0x044E"  
+	],
+	'mn-MN'=>[
+		'Culture'=> "mn-MN",
+		'Display'=> "Mongolian - Mongolia",
+		'Code'=> "0x0450"   
+	],
+	'nb-NO'=>[
+		'Culture'=> "nb-NO",
+		'Display'=> "Norwegian (Bokmål) - Norway",
+		'Code'=> "0x0414"  
+	],
+	'nn-NO'=>[
+		'Culture'=> "nn-NO",
+		'Display'=> "Norwegian (Nynorsk) - Norway",
+		'Code'=> "0x0814"  
+	],
+	'pl-PL'=>[
+		'Culture'=> "pl-PL",
+		'Display'=> "Polish - Poland",
+		'Code'=> "0x0415",
+		'ISO'=> "PLK"   
+	],
+	'pt-BR'=>[
+		'Culture'=> "pt-BR",
+		'Display'=> "Portuguese - Brazil",
+		'Code'=> "0x0416",
+		'ISO'=> "PTB"
+	],
+	'pt-PT'=>[
+    	'Culture'=> "pt-PT",
+		'Display'=> "Portuguese - Portugal",
+		'Code'=> "0x0816"
+	],
+	'pa-IN'=>[
+		'Culture'=> "pa-IN",
+		'Display'=> "Punjabi - India",
+		'Code'=> "0x0446"
+	],
+	'ro-RO'=>[
+		'Culture'=> "ro-RO",
+		'Display'=> "Romanian - Romania",
+		'Code'=> "0x0418",
+		'ISO'=> "ROM" 
+	],
+	'ru-RU'=>[
+    	'Culture'=> "ru-RU",
+		'Display'=> "Russian - Russia",
+		'Code'=> "0x0419",
+		'ISO'=> "RUS" 
+	],
+	'sa-IN'=>[
+		'Culture'=> "sa-IN",
+		'Display'=> "Sanskrit - India",
+		'Code'=> "0x044F"   
+	],
+	'Cy-sr-SP'=>[
+    	'Culture'=> "Cy-sr-SP",
+		'Display'=> "Serbian (Cyrillic) - Serbia",
+		'Code'=> "0x0C1A" 
+	],
+	'Lt-sr-SP'=>[
+    	'Culture'=> "Lt-sr-SP",
+		'Display'=> "Serbian (Latin) - Serbia",
+		'Code'=> "0x081A"
+	],
+	'sk-SK'=>[
+		'Culture'=> "sk-SK",
+		'Display'=> "Slovak - Slovakia",
+		'Code'=> "0x041B",
+		'ISO'=> "SKY"
+	],
+	'sl-SI'=>[
+    	'Culture'=> "sl-SI",
+		'Display'=> "Slovenian - Slovenia",
+		'Code'=> "0x0424",
+		'ISO'=> "SLV"
+	],
+	'es-AR'=>[
+    	'Culture'=> "es-AR",
+		'Display'=> "Spanish - Argentina",
+		'Code'=> "0x2C0A",
+		'ISO'=> "ESS"
+	],
+	'es-BO'=>[
+    	'Culture'=> "es-BO",
+		'Display'=> "Spanish - Bolivia",
+		'Code'=> "0x400A",
+		'ISO'=> "ESB"
+	],
+	'es-CL'=>[
+		'Culture'=> "es-CL",
+		'Display'=> "Spanish - Chile",
+		'Code'=> "0x340A",
+		'ISO'=> "ESL" 
+	],
+	'es-CO'=>[
+    	'Culture'=> "es-CO",
+		'Display'=> "Spanish - Colombia",
+		'Code'=> "0x240A",
+		'ISO'=> "ESO"
+	],
+	'es-CR'=>[
+    	'Culture'=> "es-CR",
+		'Display'=> "Spanish - Costa Rica",
+		'Code'=> "0x140A",
+		'ISO'=> "ESC"
+	],
+	'es-DO'=>[
+    	'Culture'=> "es-DO",
+		'Display'=> "Spanish - Dominican Republic",
+		'Code'=> "0x1C0A",
+		'ISO'=> "ESD"  
+	],
+	'es-EC'=>[
+    	'Culture'=> "es-EC",
+		'Display'=> "Spanish - Ecuador",
+		'Code'=> "0x300A",
+		'ISO'=> "ESF"
+	],
+	'es-SV'=>[
+    	'Culture'=> "es-SV",
+		'Display'=> "Spanish - El Salvador",
+		'Code'=> "0x440A",
+		'ISO'=> "ESE" 
+	],
+	'es-GT'=>[
+    	'Culture'=> "es-GT",
+		'Display'=> "Spanish - Guatemala",
+		'Code'=> "0x100A",
+		'ISO'=> "ESG" 
+	],
+	'es-HN'=>[
+		'Culture'=> "es-HN",
+		'Display'=> "Spanish - Honduras",
+		'Code'=> "0x480A",
+		'ISO'=> "ESH"   
+	],
+	'es-MX'=>[
+    	'Culture'=> "es-MX",
+		'Display'=> "Spanish - Mexico",
+		'Code'=> "0x080A",
+		'ISO'=> "ESM"  
+	],
+	'es-NI'=>[
+    	'Culture'=> "es-NI",
+		'Display'=> "Spanish - Nicaragua",
+		'Code'=> "0x4C0A",
+		'ISO'=> "ESI" 
+	],
+	'es-PA'=>[
+		'Culture'=> "es-PA",
+		'Display'=> "Spanish - Panama",
+		'Code'=> "0x180A",
+		'ISO'=> "ESA"   
+	],
+	'es-PY'=>[
+		'Culture'=> "es-PY",
+		'Display'=> "Spanish - Paraguay",
+		'Code'=> "0x3C0A",
+		'ISO'=> "ESZ"   
+	],
+	'es-PE'=>[
+		'Culture'=> "es-PE",
+		'Display'=> "Spanish - Peru",
+		'Code'=> "0x280A",
+		'ISO'=> "ESR"
+	],
+	'es-PR'=>[
+		'Culture'=> "es-PR",
+		'Display'=> "Spanish - Puerto Rico",
+		'Code'=> "0x500A",
+		'ISO'=> "ES"
+	],
+	'es-ES'=>[
+    	'Culture'=> "es-ES",
+		'Display'=> "Spanish - Spain",
+		'Code'=> "0x0C0A"
+	],
+	'es-UY'=>[
+		'Culture'=> "es-UY",
+		'Display'=> "Spanish - Uruguay",
+		'Code'=> "0x380A",
+		'ISO'=> "ESY" 
+	],
+	'es-VE'=>[
+		'Culture'=> "es-VE",
+		'Display'=> "Spanish - Venezuela",
+		'Code'=> "0x200A",
+		'ISO'=> "ESV"   
+	],
+	'sw-KE'=>[
+		'Culture'=> "sw-KE",
+		'Display'=> "Swahili - Kenya",
+		'Code'=> "0x0441"   
+	],
+	'sv-FI'=>[
+		'Culture'=> "sv-FI",
+		'Display'=> "Swedish - Finland",
+		'Code'=> "0x081D",
+		'ISO'=> "SVF"  
+	],
+	'sv-SE'=>[
+		'Culture'=> "sv-SE",
+		'Display'=> "Swedish - Sweden",
+		'Code'=> "0x041D"
+	],
+	'syr-SY'=>[
+		'Culture'=> "syr-SY",
+		'Display'=> "Syriac - Syria",
+		'Code'=> "0x045A"
+	],
+	'ta-IN'=>[
+		'Culture'=> "ta-IN",
+		'Display'=> "Tamil - India",
+		'Code'=> "0x0449" 
+	],
+	'tt-RU'=>[
+		'Culture'=> "tt-RU",
+		'Display'=> "Tatar - Russia",
+		'Code'=> "0x0444"
+	],
+	'te-IN'=>[
+		'Culture'=> "te-IN",
+		'Display'=> "Telugu - India",
+		'Code'=> "0x044A" 
+	],
+	'th-TH'=>[
+		'Culture'=> "th-TH",
+		'Display'=> "Thai - Thailand",
+		'Code'=> "0x041E",
+		'ISO'=> "THA"
+	],
+	'tr-TR'=>[
+		'Culture'=> "tr-TR",
+		'Display'=> "Turkish - Turkey",
+		'Code'=> "0x041F",
+		'ISO'=> "TRK"
+	],
+	'uk-UA'=>[
+		'Culture'=> "uk-UA",
+		'Display'=> "Ukrainian - Ukraine",
+		'Code'=> "0x0422",
+		'ISO'=> "UKR"
+	],
+	'ur-PK'=>[
+		'Culture'=> "ur-PK",
+		'Display'=> "Urdu - Pakistan",
+		'Code'=> "0x0420",
+		'ISO'=> "URD"
+	],
+	'Cy-uz-UZ'=>[
+		'Culture'=> "Cy-uz-UZ",
+		'Display'=> "Uzbek (Cyrillic) - Uzbekistan",
+		'Code'=> "0x0843"
+	],
+	'Lt-uz-UZ'=>[
+		'Culture'=> "Lt-uz-UZ",
+		'Display'=> "Uzbek (Latin) - Uzbekistan",
+		'Code'=> "0x0443",
+	],
+	'vi-VN'=>[
+		'Culture'=> "vi-VN",
+		'Display'=> "Vietnamese - Vietnam",
+		'Code'=> "0x042A",
+		'ISO'=> "VIT"
+	]
+
+];
---
+++ src/Culture.php
@@ -0,0 +1,10 @@
+<?php
+namespace Jiny\Locale;
+
+class Culture
+{
+    public function __construct()
+    {
+        echo __CLASS__."를 생성합니다.<br>";
+    }
+}
